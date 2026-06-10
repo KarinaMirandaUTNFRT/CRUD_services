15e44b2fc6e704b7917d3ee642419f97dac1dcc1
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-10 10:42:18 -0300

    correcciones de sintaxis del codigo en config.js

--- src/server/config.js
+++ src/server/config.js
@@ -4,15 +4,31 @@ import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-export default class Server{
-            constructor(){
-                this.app = express();
-                this.PORT = process.env.PORT ||  3000
-            }
+export default class Server {
+    constructor() {
+        this.app = express();
+        this.PORT = process.env.PORT || 3000;
+        
+      
+        this.middleware();
+    }
 
-Listen(){
-    app.listen(PORT, () => {
-    console.info(`servidor activo en el puerto http://localhosst:${this.PORT}`);
-});
-}
+ 
+    middleware() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(morgan('dev'));
+        
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        console.log(__dirname + '/public');
+
+        this.app.use(express.static(__dirname + '../../public'));
+    }
+
+    Listen() {
+        
+        this.app.listen(this.PORT, () => {
+            console.info(`Servidor activo en el puerto: http://localhost:${this.PORT}`);
+        });
+    }
 }
