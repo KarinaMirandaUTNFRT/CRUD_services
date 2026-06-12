672f84371f68db6b842f1221d7e2ea31ff49707c
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-11 22:38:16 -0300

    corrigio sintaxis en database

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 const conectarDb = async () => {
+  // Ponemos la URL directamente como un string para saltarnos el problema del .env
+  const URI = "mongodb://KarinaMiranda_db_user:dMrrNuEqnAtUZBqi@ac-kfegklu-shard-00-00.gjmwsad.mongodb.net:27017,ac-kfegklu-shard-00-01.gjmwsad.mongodb.net:27017,ac-kfegklu-shard-00-02.gjmwsad.mongodb.net:27017/db_servicios?ssl=true&replicaSet=atlas-glbtdp-shard-0&authSource=admin&appName=Cluster0";
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.info("¡Conexión a la base de datos exitosa!");
+    await mongoose.connect(URI);
+    console.info("¡Conexión a la base de datos exitosa mediante URI directa!");
   } catch (error) {
     console.error("Error al conectar a la base de datos:", error);
   }
 };
 
-// Exportamos por defecto para que Server controle cuándo llamarla
-export default conectarDb;
+export default conectarDb; 
