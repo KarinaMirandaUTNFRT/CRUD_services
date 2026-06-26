7e2363047b26c0e08922c07c5895293afc57ec2f
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-26 19:44:32 -0300

    creamos el modelo usuario

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -18,6 +18,7 @@ const usuarioSchema = new Schema(
       validate: {
         validator: (valor) => {
             /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(valor);
+            
         }
       }
     },
