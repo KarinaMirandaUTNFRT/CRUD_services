7d471ae0d1dbad30cc61d0d3648e51abb9f22732
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-26 19:44:32 -0300

    creamos el modelo usuario

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,3 +1,36 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 const servicioSchema = new Schema(
-    
+  {
+    nombre: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 100,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // No permite correos duplicados en la base de datos
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    rol: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["admin", "cliente"],
+      },
+    },
+  },
+
+  {
+    timestamp: true, //tengo la fecha y hora de creacion y actualizacion
+  },
+);
+const Usuario = mongoose.model("usuario", usuarioSchema); //vinculo elusuarioSchema con la DB
+export default Usuario;
