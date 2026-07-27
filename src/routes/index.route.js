236d1f08033f8e6732c6866135d3fd5b72f1f3cb
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-27 18:05:49 -0300

    se agrego el modelo  Categoria

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import serviciosRouter from "./servicios.route.js";
 import usuariosRouter from "./usuarios.route.js";
+import categoriaRouter from "./categoria.route.js";
 
 const router = Router();
 router.use("/servicios", serviciosRouter);
 router.use("/usuarios", usuariosRouter);
+router.use("/categorias", categoriaRouter);
 
 export default router;
