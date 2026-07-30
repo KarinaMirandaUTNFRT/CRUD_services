c3b68ba60be55eafe95bc3c6f3f251fe3f80a65a
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-30 14:18:00 -0300

    cambio el nombre de la ruta en el index.js por api

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import Server from "./src/server/config.js";
 //instanciar la clase Server
 
 const server = new Server();
-server.app.use('/tarea', router);
+server.app.use('/api', router);
 server.Listen();
