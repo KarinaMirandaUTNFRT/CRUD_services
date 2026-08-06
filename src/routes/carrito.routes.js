b7e349b071bffd54a2b1a9f9d60e8124dbb00546
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-08-06 15:00:41 -0300

    se agrego la resta de un producto en el controlador carrito

--- src/routes/carrito.routes.js
+++ src/routes/carrito.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   agregarAlCarrito,
   obtenerCarrito,
+  restarCantidad,
   vaciarCarrito,
 } from "../controllers/carrito.controllers.js";
 import { autenticador } from "../middlewares/authmiddleware.js";
@@ -13,5 +14,7 @@ router
   .post(autenticador, agregarAlCarrito)
   .get(autenticador, obtenerCarrito)
   .delete(autenticador, vaciarCarrito);
+
+router.route("/restar/:servicioId").patch(autenticador, restarCantidad);
 
 export default router;
