aae762790a5b79972ebaa9da8fb051c611faa775
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-08-21 18:15:08 -0300

    se cambia el nombre de la ruta de crearPreferenciaPago a crearPreferencia

--- src/routes/pago.route.js
+++ src/routes/pago.route.js
@@ -5,7 +5,7 @@ import { autenticador } from "../middlewares/authmiddleware.js";
 
 const router = Router();
 
-router.route("/crear-preferencia"). post(autenticador, crearPreferenciaPago)
+router.route("/crear-preferencia"). post(autenticador, crearPreferencia)
   
 
 
