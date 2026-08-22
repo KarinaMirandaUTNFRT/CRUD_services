cc79d101ddeb0bcb089a3fb7a522e88549bdd11b
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-08-21 21:52:22 -0300

    se corrige la ruta webhook

--- src/routes/pago.route.js
+++ src/routes/pago.route.js
@@ -5,7 +5,7 @@ import { autenticador } from "../middlewares/authmiddleware.js";
 
 const router = Router();
 
-router.route("/crear-preferencia"). post(autenticador, crearPreferenciaPago)
-router.route("/webhook", recibirWebhook);
+router.route("/crear-preferencia") . post(autenticador, crearPreferenciaPago)
+router.post("/webhook", recibirWebhook);
 
 export default router;
